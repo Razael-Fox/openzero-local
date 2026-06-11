c06bc80a330e061f1e05211099ddd7b8136e5ad0
razaeldotexe <razaelsaputra863@gmail.com>
2026-06-11 13:05:28 +0700

    chore: configure CSP frame-ancestors in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://razael-fox.my.id https://*.vercel.app localhost:* http://localhost:*",
+          },
+        ],
+      },
+    ];
+  },
+});
